368de77db51ab76deffa7822cdfc17a53609468f
Audomaro Glez <jose_audomaro@hotmail.com>
2024-05-03 08:08:19 -0500

    chore: :zap: update config

--- MusicalTrack/src/test/java/com/expeditors/musicaltrack/providers/PriceRestClientProviderTest.java
+++ MusicalTrack/src/test/java/com/expeditors/musicaltrack/providers/PriceRestClientProviderTest.java
@@ -2,23 +2,39 @@ package com.expeditors.musicaltrack.providers;
 
 import com.expeditors.musicaltrack.domain.Track;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doAnswer;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PriceRestClientProviderTest {
 
-    @Autowired
+    @Mock
     private PriceRestClientProvider provider;
 
     @Test
     void addPrice() {
         Track track = new Track();
 
+        double price = 1.25;
+
+        doAnswer(invocation -> {
+            Track trackArgument = invocation.getArgument(0);
+            trackArgument.setPrice(price);
+            return null;
+        }).when(provider).addPrice(track);
+
         provider.addPrice(track);
 
+        assertEquals(track.getPrice(), price);
+
         assertTrue(track.getPrice() > 0);
+
+        Mockito.verify(provider).addPrice(track);
     }
 }
